1bc910e9dc2a0b08b98eb3a20c22a9920eb2e8e8
shpandey <shivampandey91198@gmail.com>
2021-12-09 09:01:56 +0530

    Add component file and update index file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import Primary from './myComponent/Primary';
+import TodoAdd from './myComponent/TodoAdd';
+import TodoList from './myComponent/TodoList';
+export default function Lin(){
+  return(
+      <Router>
+          
+          <div>
+              <Link to ='/'></Link>
+          </div>
+          <div >
+              <Link to ='/todoadd'></Link>
+          </div>
+          <div >
+              <Link to ='/todolist'></Link>
+          </div>
+          <Switch>
+              <Route exact path="/">
+                 <Primary />
+              </Route>  
+              <Route exact path='/todoadd'>
+                  <TodoAdd/>
+              </Route>     
+              <Route exact path='/todolist'>
+                  <TodoList/>
+              </Route>            
+          </Switch> 
+      </Router>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Lin/>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
