abd0c5ae1e9e9d2facd07bd855a66bdead349788
shpandey <shivampandey91198@gmail.com>
2021-12-09 11:13:04 +0530

    Edit app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import react from 'react';
 import './App.css';
 import KK from './component/kk.js';
+import {BrowserRouter as Route, Router, Link, Switch} from 'react-router-dom';
 function App() {
   return (
-    <div>
-      Hello React
-    <KK />
-    </div>
+    <Router>
+      <div>
+        <Link to = "/">Main</Link>
+      </div>
+      <Switch>
+        <Route path="/">
+          <KK/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
