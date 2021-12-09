a3a2e1e7f01083ed8a6cc1239f9e501d16e07aa3
shpandey <shivampandey91198@gmail.com>
2021-12-09 11:06:36 +0530

    Edit app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-//import logo from './logo.svg';
+import react from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Primary from './myComponent/Primary';
-import TodoAdd from './myComponent/TodoAdd';
-import TodoList from './myComponent/TodoList';
+import KK from './component/kk.js';
 function App() {
   return (
-    <Router>
-          
     <div>
-        <Link to ='/'></Link>
+      Hello React
+    <KK />
     </div>
-    <div >
-        <Link to ='/todoadd'></Link>
-    </div>
-    <div >
-        <Link to ='/todolist'></Link>
-    </div>
-    <Switch>
-        <Route exact path="/">
-           <Primary />
-        </Route>  
-        <Route exact path='/todoadd'>
-            <TodoAdd/>
-        </Route>     
-        <Route exact path='/todolist'>
-            <TodoList/>
-        </Route>            
-    </Switch> 
-</Router>
   );
 }
 
---
+++ src/component/kk.js
@@ -0,0 +1,8 @@
+import react from "react";
+export default function Lin(){
+    return(
+        <div>
+            <h1>Hello Suraj</h1>
+        </div>
+    );
+}
